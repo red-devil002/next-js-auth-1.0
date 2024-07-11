2e9653ae5e7e270931818a3f79ce963bbb67099f
Swetang Pandit <swetangpandit@gmail.com>
2024-07-11 16:22:43 +0530

    Logout Route

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,23 @@
+import { connectDB } from "@/dbConnection/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB()
+
+export async function POST(req: NextRequest) {
+    try {
+        const response = NextResponse.json({
+            message: "Logout Successfully",
+            success: true
+        })
+
+        response.cookies.set("token", "", {
+            httpOnly: true,
+            expires: new Date(0)
+        })
+
+        return response
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
